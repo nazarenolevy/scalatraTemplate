62553c812a02c0d2e65ff3a68e86dcf2d31abaf2
nazarenolevy <nazareno.levy@despegar.com>
2017-10-25 14:20:39 -0300

    unit test for config reader

--- src/test/unit/com.example.app.common.util/ConfigReaderTest.scala
+++ src/test/unit/com.example.app.common.util/ConfigReaderTest.scala
@@ -2,16 +2,11 @@ package com.example.app.common.util
 
 import com.example.app.UnitTest
 import com.example.app.common.logging.Logging
-import com.typesafe.config.ConfigFactory
 import org.scalatest.FunSuiteLike
 import net.ceedubs.ficus.Ficus._
 import org.scalatra.test.scalatest.ScalatraSuite
 
 class ConfigReaderTest extends UnitTest with ScalatraSuite with FunSuiteLike with Logging with ConfigReader {
-
-  private def overrideConfig(overrideValues: Map[String, Any]) = {
-    ConfigReader.reloadConfig(overrideValues)
-  }
 
   test("Should read the configuration"){
     envConfiguration.as[String]("environment") shouldBe "test"
@@ -21,7 +16,5 @@ class ConfigReaderTest extends UnitTest with ScalatraSuite with FunSuiteLike wit
     ConfigReader.reloadConfig(Map("environment" -> "bla"))
     envConfiguration.as[String]("environment") shouldBe "bla"
   }
-
-
 
 }
